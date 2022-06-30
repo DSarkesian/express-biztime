e47839e22a3a795d4fc8b4d8154676f0642962d1
anela <anela89@gmail.com>
2022-06-30 16:08:18 -0700

    added GET /invoices/:id route

--- db.js
+++ db.js
@@ -2,9 +2,15 @@
 /** Database setup for BizTime. */
 const { Client } = require("pg");
 
+/** MAC */
+// const DB_URI = process.env.NODE_ENV === "test"
+//     ? "postgresql:///biztime_test"
+//     : "postgresql:///biztime";
+
+/** Ham's PC */
 const DB_URI = process.env.NODE_ENV === "test"
-    ? "postgresql:///biztime_test"
-    : "postgresql:///biztime";
+    ? "postgresql://hannahanela:foofoo@localhost/biztime_test"
+    : "postgresql://hannahanela:foofoo@localhost/biztime";
 
 let db = new Client({
   connectionString: DB_URI
--- routes/invoices.js
+++ routes/invoices.js
@@ -1,2 +1,44 @@
 "use strict"
-//const db = require("../db");
+const express = require("express");
+const db = require("../db");
+
+const { NotFoundError, BadRequestError } = require("../expressError");
+const invoicesRouter = new express.Router();
+
+/** GET/ - returns all invoices `{invoices:[{id, comp_code},...]} */
+invoicesRouter.get("/", async function (req, res) {
+    const results = await db.query("SELECT id, comp_code FROM invoices");
+    const invoices = results.rows;
+  
+    return res.json({ invoices });
+  });
+
+module.exports = invoicesRouter;
+
+/** GET/[id] - return data about one invoice
+ * 
+ * {invoice: {id, amt, paid, add_date, paid_date,
+ *      company: {code, name, description}} */
+
+ invoicesRouter.get("/:id", async function (req, res) {
+    const id = req.params.id;
+  
+    const results = await db.query(
+      `SELECT id, comp_code, amt, paid, add_date, paid_date
+                  FROM invoices
+                  WHERE id = $1`, [id]);
+    const invoice = results.rows[0];
+
+    if (!invoice) throw new NotFoundError(`No matching invoice: ${id}`);
+
+    const cResults = await db.query(
+        `SELECT code, name, description
+                    FROM companies
+                    WHERE code = $1`, [invoice.comp_code]);
+    const company = cResults.rows[0];
+
+    invoice.company = company;
+    delete invoice.comp_code;
+  
+    return res.json({ invoice });
+  });
