e0cc138852df57068a51352736c6801d8c24bbdf
Daniel Sarkesian <danielsarkesian@gmail.com>
2022-06-30 15:02:22 -0400

    worked on first route

---
+++ routes/companies.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const db = require("../db");
+const { NotFoundError, BadRequestError } = require("../expressError");
+const companiesRouter = new express.Router();
+
+companiesRouter.get("/", async function (req,res){
+  const results = await db.query("SELECT code, name FROM companies");
+  const companies = results.rows;
+  console.log("is the route working?")
+
+  return res.json({companies})
+})
+
+
+
+companiesRouter.delete("/:id", async function (req, res, next) {
+  const id = req.params.id;
+  const results = await db.query(
+    "DELETE FROM cats WHERE id = $1 RETURNING id", [id]);
+  const cat = results.rows[0];
+
+  if (!cat) throw new NotFoundError(`No matching cat: ${id}`);
+  return res.json({ message: "Cat deleted" });
+});
+
+module.exports = companiesRouter;
