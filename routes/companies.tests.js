92b8fead9d4b68d63f619f81f52b4b970af2d6c6
anela <anela89@gmail.com>
2022-06-30 17:21:44 -0700

    started writing tests for companies.js

---
+++ routes/companies.tests.js
@@ -0,0 +1,29 @@
+const request = require("supertest");
+
+// app imports
+const app = require("../app");
+const db = require("../db");
+
+let testCompanies;
+
+beforeEach(async function() {
+    await db.query("DELETE FROM companies");
+    let result = await db.query(`
+    INSERT INTO companies (code, name, description)
+    VALUES ('tcomp', 'TestCompany', "Test description")
+    RETURNING code, name, description`);
+    testCompanies = result.rows[0];
+});
+
+describe("GET/companies/", function() {
+    testCompanies("Gets list of companies", async function() {
+        const resp = await request(app).get(`/companies/`);
+        expect(resp.body).toEqual({ company: [{testCompanies}]});
+    })
+});
+
+afterAll(async function () {
+    // close db connection --- if you forget this, Jest will hang
+    await db.end();
+  });
+  
